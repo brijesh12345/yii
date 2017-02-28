b88bc58e3d8dc33a2a7bb1d9a1ce43122a69eb31
Niraj Kumar <niraj.kumar@laitkor.com>
2017-02-28 15:01:24 +0530

    first commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'm2n1shlko',
+    'charset' => 'utf8',
+];
